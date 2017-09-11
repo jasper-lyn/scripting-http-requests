835eda722fcef000db4d94135abadfa129f1de12
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-09-11 16:35:04 +0000

    printing with parameters

---
+++ step-3.js
@@ -0,0 +1,23 @@
+function getAndPrintHTML (options) {
+
+  var http = require('http');
+
+  http.get(options, function (response) {
+    response.setEncoding('utf8');
+    var eachChunk = "";
+    response.on('data', function (data) {
+      for (i = 0; i < data.length; i++) {
+        eachChunk += data[i];
+      }
+      console.log(eachChunk);
+    });
+  });
+}
+
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+console.log(getAndPrintHTML(requestOptions));
